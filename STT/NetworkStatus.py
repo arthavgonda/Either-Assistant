a60a21ef5ea109870a898eae739a752f27ea670b
Anoop Singh Negi <139070191+arthavgonda@users.noreply.github.com>
2025-12-24 00:40:40 +0530

    Delete STT directory

--- STT/NetworkStatus.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-def check_server_connectivity(host="HOST_ADDRESS", port = 80, timeout = 3):
-    try:
-        with socket.create_connection((host, port), timeout):
-            return True
-    except (socket.timeout, ConnectionRefusedError, OSError):
-        return False
